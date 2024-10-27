1b1edc13e6119532177615fdfef5fd4fcf737554
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-27 11:33:27 -0700

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-import WarehouseGame from './components/WarehouseGame'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-4 sm:p-8">
-      <WarehouseGame />
-    </main>
-  )
-}
